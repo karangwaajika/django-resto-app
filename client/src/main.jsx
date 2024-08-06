a5aa1bbaca38ae6718a952e73b39da88f0143de7
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-08-06 15:33:43 +0300

    feat: set up working directory

---
+++ client/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "../assets/App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <p>Home</p>
+    </div>
+  );
+}
+
+export default App;
