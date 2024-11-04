0aa7cc5e8ed2b84c2412eb349a131eb7540d5eef
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-11-04 09:54:23 +0200

    chor: switch database from sqlite to postgres

--- server/resto/settings.py
+++ server/resto/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'resto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Alsina@post',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
