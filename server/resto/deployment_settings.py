e3af444c2cd635164c8c4325d5544bb29c7bec1d
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-11-04 10:46:09 +0200

    ops: create deployment settings.py

---
+++ server/resto/deployment_settings.py
@@ -0,0 +1,75 @@
+import os 
+import dj_database_url
+from .settings import * 
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+
+DEBUG = False
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# frontend url
+CORS_ALLOWED_ORIGINS = [
+    'https://render-deploy-tutorial-reactjs-code.onrender.com'
+]
+
+STORAGES = {
+    "default":{
+        "BACKEND" : "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+
+}
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default= os.environ['DATABASE_URL'], 
+        conn_max_age=600
+    )
+}
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+
+
+ADMINS = [("CBI Analytics", "YOUREMAIL@EMAIL.com")]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = 'default from email'
--- server/resto/settings.py
+++ server/resto/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# staticFiles is the name of the folder containg static files
+STATIC_ROOT = BASE_DIR/'staticFiles'
 STATIC_URL = 'static/'
 
 # Default primary key field type
