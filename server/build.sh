6e64b4c36ac0a43b40b96bccc0aba52f2536ef23
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-11-05 08:12:29 +0200

    ops: create superuser inside build.sh

--- server/build.sh
+++ server/build.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-# if [[ $CREATE_SUPERUSER ]]
-# then
-#     python manage.py createsuperuser --no-input
-# fi
+if [[ $CREATE_SUPERUSER ]]
+then
+    python manage.py createsuperuser --no-input
+fi
