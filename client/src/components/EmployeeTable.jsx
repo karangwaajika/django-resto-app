b59197d02dcb4cfa103bd69aeba74c809e1da9c7
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-11-06 11:26:22 +0200

    feat: display and search employee information

---
+++ client/src/components/EmployeeTable.jsx
@@ -0,0 +1,55 @@
+import { formatToDateString } from "../utils/dateFormat.mjs";
+export default function EmployeeTable({ employees }) {
+  return (
+    <table className="waiter-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Names</th>
+          <th>Join on</th>
+          <th>Role/Edit</th>
+          <th>Active/Edit</th>
+          <th>Set Password</th>
+        </tr>
+      </thead>
+      <tbody>
+        {employees.length > 0 ? (
+          employees.map((employee, index) => {
+            return (
+              <tr key={index + 1}>
+                <td data-cell="#">{index + 1}</td>
+                <td data-cell="Name">
+                  {employee.first_name + " " + employee.last_name}
+                </td>
+                <td data-cell="Date">
+                  {formatToDateString(employee.date_joined)}
+                </td>
+                <td data-cell="Role">
+                  {employee.is_staff ? "Bartender" : "Waiter"}{" "}
+                  <i className="fa fa-pencil"></i>
+                </td>
+                <td data-cell="Active" style={{ textAlign: "center" }}>
+                  {employee.is_active ? (
+                    <i className="fa fa-check-square text-success"></i>
+                  ) : (
+                    <i className="fa fa-square-xmax text-danger"></i>
+                  )}{" "}
+                  <i className="fa fa-pencil"></i>
+                </td>
+                <td data-cell="Password">
+                  <i className="fa fa-pencil"></i>
+                </td>
+              </tr>
+            );
+          })
+        ) : (
+          <tr>
+            <td colSpan={6} style={{ textAlign: "center", color: "red" }}>
+              No data
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+}
--- client/src/pages/ViewWaiters.jsx
+++ client/src/pages/ViewWaiters.jsx
@@ -1,3 +1,43 @@
-export default function ViewWaiters() {
-  return <>{/* todo: view waiter list */}</>;
+import InputField from "../components/ui/InputField";
+import loaderPicture from "/images/loading-3.gif";
+import { useState } from "react";
+import useFetchAutoComplete from "../hooks/useFetchAutoComplete";
+import EmployeeTable from "../components/EmployeeTable";
+
+export default function ViewBeverages() {
+  // handle fetch auto complete
+  const [search, setSearch] = useState("");
+  const isDevelopment = import.meta.env.MODE === "production";
+  const url = isDevelopment
+    ? import.meta.env.VITE_REACT_APP_VIEW_EMPLOYEES_API_DEPLOY
+    : import.meta.env.VITE_REACT_APP_VIEW_EMPLOYEES_API;
+  const { data, isLoading, message } = useFetchAutoComplete(url, search);
+
+  return (
+    <div className="view-beverage-content">
+      <div className="beverage-header">
+        <h2>List of Employees</h2>
+        <p style={{ fontSize: "14px" }}>
+          Search by names to retrieve the desired information
+        </p>
+      </div>
+      <div className="search-btn">
+        <InputField
+          type="search"
+          name="search"
+          id="search"
+          label="Search"
+          icon="fa-solid fa-search"
+          placeholder="Search ... "
+          handleChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      {isLoading && (
+        <div className="loader">
+          <img src={loaderPicture} width={100} height={100} />
+        </div>
+      )}
+      <EmployeeTable employees={data} />
+    </div>
+  );
 }
