22ff28313abe5a9b95b9bf81a9226baead2f5cf2
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-11-15 17:55:48 +0200

    feat: insert order details

--- server/service/views.py
+++ server/service/views.py
@@ -13,6 +13,7 @@ from user.serializers import *
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 import datetime
+import pytz
 
 
 # Create your views here.
@@ -21,17 +22,16 @@ import datetime
 @permission_classes([IsAuthenticated])
 def get_last_order(request):
     last_order = Order.objects.last()
-    user = UserSerializer(request.user)
     if last_order:
         return Response(
             {
                 "success": True,
                 "message": "Service App here!",
-                "data": {"order_id": last_order.id},
+                "data": {"order_id": last_order.id + 1},
             }
         )
     return Response(
-        {"success": True, "message": "Service App here!", "user":{"info":user.data} ,"data": {"order_id": 1}}
+        {"success": True, "message": "Service App here!", "data": {"order_id": 1}}
     )
 
 
@@ -40,12 +40,23 @@ def get_last_order(request):
 @permission_classes([IsAuthenticated])
 def record_order(request):
     order_details = request.data
-    order_id, order_type,customer_name,beverages, meals, teas = order_details.values()
-    date_today = datetime.datetime.now()
+    order_id, order_type, customer_name, beverages, meals, teas = order_details.values()
+    user = request.user
+    dt_now = datetime.datetime.now(tz=pytz.UTC)
+    date_today = dt_now.astimezone(pytz.timezone("Africa/Kigali"))
     
+    # insert order
+    add_order = Order.objects.create(
+        employee=user,
+        customer_name=customer_name,
+        order_type=order_type,
+        date_time=date_today,
+        sold_date=date_today,
+    )
+
     return Response(
         {
             "success": True,
-            "message": "Service App here!",
+            "message": "Order recorded successfully",
         }
     )
