535ffee7096a407d293ee112d8da6dcb9014e211
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-12-02 14:47:25 +0200

    feat: design large device #homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,8 +11,12 @@ function App() {
       </header>
       <main className="main main-home">
         <div className="overlay">
-          <div className="login-text">
-            <h3>Welcome Back!!</h3>
+          <div className="login-text" >
+            <h3 style={{fontFamily:"cursive"}}>Welcome Back L&#128514;L Staff!!</h3>
+            <p>Login to start your daily stask!</p>
+          </div>
+          <div className="login-text-large-device" >
+            <h1 style={{fontFamily:"cursive"}}>L&#128514;L Management I.S Platform</h1>
             <p>Login to start your daily stask!</p>
           </div>
           <LoginForm />
